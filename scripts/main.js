d187ae948dab96f2350329694082935d58a2e543
Mame Coumba Ka <mck@Mames-MBP-2.attlocal.net>
2023-02-14 00:52:24 -0800

    added js

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,27 @@
-const book = document.getElementsById('book')
+const books = document.getElementById("books").childNodes;
+for (let i = 0; i < books.length; i++) {
+    books[i].addEventListener("click", function() {
+        toggleBook(books[i]);
+    });
+}
+//for each book add event listener , call toggle show/hide
+
+
+
+function toggleBook(book) {
+    var expanded = book.getAttribute("aria-expanded");
+    if (expanded ==="false") {
+        book.classList.remove("hide");
+        book.classList.add("show");
+        book.setAttribute("aria-expanded", "true");
+
+    } else {
+        book.classList.remove("show");
+        book.classList.add("hide")
+        book.setAttribute("aria-expanded", "false");
+    }
+}
+
+
+
+
